76428eb536f595d50cc1c4b98202db519eb29a2b
chihiro.shimoyoma <chihiro.shimoyoma>
2018-05-17 06:24:53 +0000

    admin login after

--- config/app.php
+++ config/app.php
@@ -159,6 +159,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        //追加
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -208,6 +211,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        //追加
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
